743d4c5f88f16808570a156f4649daf0b161dbe2
Ashen-Zhao <zhaoashen@gmail.com>
2016-12-26 10:10:55 +0800

    Site updated at 2016-12-26 02:10:55 UTC

--- her/renxi/functions.js
+++ her/renxi/functions.js
@@ -40,14 +40,14 @@ $(window).resize(function() {
 function timeElapse(date){
 	var current = Date();
 	var seconds = (Date.parse(current) - Date.parse(date)) / 1000;
-	var days = Math.floor(seconds / (3600 * 24));
+	var days = Math.ceil(seconds / (3600 * 24));
 	seconds = seconds % (3600 * 24);
-	var hours = Math.floor(seconds / 3600);
+	var hours = Math.ceil(seconds / 3600);
 	if (hours < 10) {
 		hours = "0" + hours;
 	}
 	seconds = seconds % 3600;
-	var minutes = Math.floor(seconds / 60);
+	var minutes = Math.ceil(seconds / 60);
 	if (minutes < 10) {
 		minutes = "0" + minutes;
 	}
@@ -55,6 +55,9 @@ function timeElapse(date){
 	if (seconds < 10) {
 		seconds = "0" + seconds;
 	}
-	var result = "第 <span class=\"digit\">" + days + "</span> 天 <span class=\"digit\">" + hours + "</span> 小时 <span class=\"digit\">" + minutes + "</span> 分钟 <span class=\"digit\">" + seconds + "</span> 秒"; 
+	var result = "第 <span class=\"digit\">" + days
+    + "</span> 天 <span class=\"digit\">" + hours
+    + "</span> 小时 <span class=\"digit\">" + minutes
+    + "</span> 分钟 <span class=\"digit\">" + seconds + "</span> 秒";
 	$("#clock").html(result);
 }
