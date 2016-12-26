5ed205bd2cac264800ecbc78a6f37a537237a5ff
Ashen-Zhao <zhaoashen@gmail.com>
2016-12-26 14:53:42 +0800

    Site updated at 2016-12-26 06:53:42 UTC

--- her/renxi/functions.js
+++ her/renxi/functions.js
@@ -38,16 +38,17 @@ $(window).resize(function() {
 })(jQuery);
 
 function timeElapse(date){
-	var current = Date();
-	var seconds = (Date.parse(current) - Date.parse(date)) / 1000;
-	var days = Math.ceil(seconds / (3600 * 24));
+	var current = new Date();
+    var seconds = (Date.parse(current) - Date.parse(date)) / 1000;
+    var years = Math.floor(seconds / (3600 * 24 * 365));
+	var days = Math.floor(seconds / (3600 * 24));
 	seconds = seconds % (3600 * 24);
-	var hours = Math.ceil(seconds / 3600);
+	var hours = Math.floor(seconds / 3600);
 	if (hours < 10) {
 		hours = "0" + hours;
 	}
 	seconds = seconds % 3600;
-	var minutes = Math.ceil(seconds / 60);
+	var minutes = Math.floor(seconds / 60);
 	if (minutes < 10) {
 		minutes = "0" + minutes;
 	}
@@ -55,7 +56,8 @@ function timeElapse(date){
 	if (seconds < 10) {
 		seconds = "0" + seconds;
 	}
-	var result = "第 <span class=\"digit\">" + days
+	var result = "第 <span class=\"digit\">" + years
+    + "</span> 个年头, 第 <span class=\"digit\">" + days
     + "</span> 天 <span class=\"digit\">" + hours
     + "</span> 小时 <span class=\"digit\">" + minutes
     + "</span> 分钟 <span class=\"digit\">" + seconds + "</span> 秒";
